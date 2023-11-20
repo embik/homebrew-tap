dac6c9edf8c45e3f34324c51008485c08d464926
Marvin Beckers <mail@embik.me>
2023-11-20 15:02:18 +0100

    formula/kubeconfig-bikeshed: add initial 0.1.0 formula version
    Signed-off-by: Marvin Beckers <mail@embik.me>

---
+++ Formula/kubeconfig-bikeshed.rb
@@ -0,0 +1,19 @@
+class KubeconfigBikeshed < Formula
+  desc "Opinionated tool to manage your kubeconfigs"
+  homepage "https://github.com/embik/kubeconfig-bikeshed"
+  url "https://github.com/embik/kubeconfig-bikeshed/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "b22729bd8fde18996ef01d7586654bfb08d139891f802c3d4f12712af6d8f589"
+  license "Apache-2.0"
+
+  depends_on "rust" => :build
+  depends_on "fzf" => :optional
+
+  def install
+    system "cargo", "install", "--path", "."
+    bin.install "target/release/kbs"
+  end
+
+  test do
+    system "#{bin}/kbs", "--help"
+  end
+end
