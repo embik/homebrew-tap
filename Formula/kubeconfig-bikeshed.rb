7e7c51c28a4eef71e50b8a55d80b610fbb304b6b
Marvin Beckers <mail@embik.me>
2023-11-21 10:40:40 +0100

    kubeconfig-bikeshed: bump to v0.1.2
    Signed-off-by: Marvin Beckers <mail@embik.me>

--- Formula/kubeconfig-bikeshed.rb
+++ Formula/kubeconfig-bikeshed.rb
@@ -1,8 +1,8 @@
 class KubeconfigBikeshed < Formula
   desc "Opinionated tool to manage your kubeconfigs"
   homepage "https://github.com/embik/kubeconfig-bikeshed"
-  url "https://github.com/embik/kubeconfig-bikeshed/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "d99d27bc476b19f762b8bbc321ea8ab43670520029c931139b32d4eed1e78b71"
+  url "https://github.com/embik/kubeconfig-bikeshed/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "238f1e670b0d597c5d2007e94e28004d423942ccddcaad7d74a55a549d25e75b"
   license "Apache-2.0"
 
   bottle do
