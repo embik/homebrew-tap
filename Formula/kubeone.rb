ae6598edb456d51b03c28f4ed6c7a04ebd51f498
Marvin Beckers <mail@embik.me>
2024-07-05 16:37:08 +0200

    kubeone: bump to v1.8.1
    Signed-off-by: Marvin Beckers <mail@embik.me>

--- Formula/kubeone.rb
+++ Formula/kubeone.rb
@@ -1,8 +1,8 @@
 class Kubeone < Formula
   desc "Automates Kubernetes cluster operations on any cloud or edge infrastructure"
   homepage "https://docs.kubermatic.com/kubeone/v1.8"
-  url "https://github.com/kubermatic/kubeone/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "d4279f0f49fee6db6e9ce8d88cf19d5411b69ad85f4ff4ac514b31a676a1c311"
+  url "https://github.com/kubermatic/kubeone/archive/refs/tags/v1.8.1.tar.gz"
+  sha256 "d73ce090f47ca9b14fd4e6bec3b18e1c3500c9260d083014c18d1a0ba6adabf2"
   license "Apache-2.0"
 
   head "https://github.com/kubermatic/kubeone.git", branch: "main"
@@ -20,7 +20,7 @@ class Kubeone < Formula
     ENV["CGO_ENABLED"] = "0"
     ldflags = %W[
       -s -w
-      -X k8c.io/kubeone/pkg/cmd.defaultKubeVersion=v1.29.5
+      -X k8c.io/kubeone/pkg/cmd.defaultKubeVersion=v1.30.2
       -X k8c.io/kubeone/pkg/cmd.version=v#{version}
       -X k8c.io/kubeone/pkg/cmd.date=#{time.iso8601}
     ]
